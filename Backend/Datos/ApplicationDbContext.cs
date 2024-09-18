28379906ac3f847f57a1ec24c67134098988d73b
ssergiocr01 <ssergio0@hotmail.com>
2024-09-18 09:29:10 -0600

    Iniciando Backend

---
+++ Backend/Datos/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelos.Usuario;
+using System.Reflection;
+
+namespace Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SoccerAppBd;Integrated Security=true");
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Backend/Modelos/Entidades/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Modelos.Usuario
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
